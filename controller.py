34d9147735164d3f721bf3eb6ef700125e8c84af
yindaheng98 <yindaheng98@163.com>
2021-08-18 15:53:59 +0800

    Pair完成

--- controller.py
+++ controller.py
@@ -31,7 +31,7 @@ class DownloadController:
         '''等待队列中的所有任务完成'''
         await self.__queue.join()
 
-    async def download_task(self, sem: asyncio.Semaphore):
+    async def get_task(self, sem: asyncio.Semaphore):
         '''独立运行的Download任务'''
         while True:
             item = await self.__queue.get()
@@ -60,7 +60,7 @@ class FeedController:
         except StopAsyncIteration:
             pass
 
-    async def feed_task(self, sem: asyncio.Semaphore, download_controllers: List[DownloadController]):
+    async def get_task(self, sem: asyncio.Semaphore, download_controllers: List[DownloadController]):
         '''独立运行的Feed任务'''
         async for item in self.__get_feeds(sem):  # 以固定并发数获取待下载项目
             if item is None:
@@ -76,30 +76,30 @@ class Pair:
     '''feeder-downloader对'''
 
     def __init__(self):
-        self.__feeders: List[Feeder] = []
-        self.__downloaders: List[Downloader] = []
+        self.__fcs: List[FeedController] = []
+        self.__dcs: List[DownloadController] = []
 
         # 一次下载全部完成后，经过多长时间开始下一次下载
         self.__timedelta: timedelta = timedelta(minutes=30)
 
         # Semaphore信号量是asyncio提供的控制协程并发数的方法
-        self.__feeder_sem: asyncio.Semaphore = asyncio.Semaphore(3)
-        self.__downloader_sem: asyncio.Semaphore = asyncio.Semaphore(3)
+        self.__fc_sem: asyncio.Semaphore = asyncio.Semaphore(3)
+        self.__dc_sem: asyncio.Semaphore = asyncio.Semaphore(3)
 
         # 每个下载器都需要一个队列
         self.__queues: List[asyncio.Queue] = []
 
     def add_feeder(self, feeder: Feeder):
-        self.__feeders.append(feeder)
+        self.__fcs.append(FeedController(feeder))
 
     def add_feeders(self, feeders: List[Feeder]):
-        self.__feeders.extend(feeders)
+        self.__feeders.extend([FeedController(feeder) for feeder in feeders])
 
     def add_downloader(self, downloader: Downloader):
-        self.__downloaders.append(downloader)
+        self.__downloaders.append(DownloadController(downloader))
 
     def add_downloaders(self, downloaders: List[Downloader]):
-        self.__downloaders.extend(downloaders)
+        self.__downloaders.extend([DownloadController(downloader) for downloader in downloaders])
 
     def set_timedelta(self, timedelta: timedelta):
         self.__timedelta = timedelta
@@ -110,9 +110,20 @@ class Pair:
     def set_downloader_concurrency(self, n: int):
         self.__downloader_sem = asyncio.Semaphore(n)
 
-    async def get_task():
-        '''独立运行的Feed&Download任务'''
-        pass  # TODO
+    def get_tasks(self):
+        '''聚合独立运行的Feed&Download任务'''
+        tasks = []
+        tasks.extend([fc.get_task(self.__fc_sem, self.__dcs) for fc in self.__fcs])
+        tasks.extend([dc.get_task(self.__fc_sem) for dc in self.__dcs])
+        return tasks
+
+    async def run_once(self):
+        await asyncio.gather(self.get_tasks())
+
+    async def run_forever(self):
+        while await asyncio.sleep(self.__timedelta.total_seconds(), result=True):
+            await self.run_once()
+    
 
 
 class Controller:
