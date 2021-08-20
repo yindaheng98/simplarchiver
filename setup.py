0080c6f730356d815e26810338c6a13d1a647689
yindaheng98 <yindaheng98@163.com>
2021-08-20 21:38:24 +0800

    版本更新

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name='simplarchiver',
-    version='1.0.0',
+    version='1.0.1',
     author='yindaheng98',
     author_email='yindaheng98@163.com',
     url='https://github.com/yindaheng98/simplarchiver',
