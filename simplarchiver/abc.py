79f206e04ece4fec0fdff59cf7b7011ba9ca996b
yindaheng98 <yindaheng98@163.com>
2021-08-20 15:19:54 +0800

    增加可选的item过滤功能

--- simplarchiver/abc.py
+++ simplarchiver/abc.py
@@ -1,14 +1,60 @@
 import abc
-import asyncio
 
 
 class Feeder(metaclass=abc.ABCMeta):
+    """最基本的Feeder"""
     @abc.abstractmethod
     async def get_feeds(self):
         yield
 
 
 class Downloader(metaclass=abc.ABCMeta):
+    """最基本的Downloader"""
     @abc.abstractmethod
     async def download(self, item):
         pass
+
+
+class FilterFeeder(Feeder):
+    """带过滤功能的Feeder"""
+
+    def __init__(self, base_feeder: Feeder):
+        """从一个基本的Feeder生成带过滤的Feeder"""
+        self.__base_feeder = base_feeder
+
+    @abc.abstractmethod
+    async def filter(self, item):
+        """
+        如果过滤器返回了None，则会被过滤掉，不会被yield
+        过滤器内可以修改item
+        """
+        return item
+
+    async def get_feeds(self):
+        """带过滤的Feeder的Feed过程"""
+        async for item in self.__base_feeder.get_feeds():
+            item = self.filter(item)
+            if item is not None:  # 如果过滤器返回了None，则会被过滤掉，不会被yield
+                yield item
+
+
+class FilterDownloader(Downloader):
+    """带过滤功能的Downloader"""
+
+    def __init__(self, base_downloader: Downloader):
+        """从一个基本的Downloader生成带过滤的Downloader"""
+        self.__base_downloader = base_downloader
+
+    @abc.abstractmethod
+    async def filter(self, item):
+        """
+        如果过滤器返回了None，则会被过滤掉，不会被Download
+        过滤器内可以修改item
+        """
+        return item
+
+    async def download(self, item):
+        """带过滤的Downloader的Download过程"""
+        item = await self.filter(item)
+        if item is not None:  # 如果过滤器返回了None，则会被过滤掉，不会被Download
+            return await self.__base_downloader.download(item)
