de152e131c7b9ce0bb3d9c04a96d570c4e987fd2
yindaheng98 <yindaheng98@163.com>
2021-08-21 11:06:02 +0800

    上传example包

--- setup.py
+++ setup.py
@@ -8,14 +8,18 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name='simplarchiver',
-    version='1.0.3',
+    version='1.0.4',
     author='yindaheng98',
     author_email='yindaheng98@163.com',
     url='https://github.com/yindaheng98/simplarchiver',
     description=u'一个简单的可扩展聚合异步下载器框架',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['simplarchiver'],
+    package_dir={
+        'simplarchiver': 'simplarchiver',
+        'simplarchiver.example': 'simplarchiver/example'
+    },
+    packages=['simplarchiver', 'simplarchiver.example'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
