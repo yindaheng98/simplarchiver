0bf2ffe27fa71160c7bd14227d7e309459d4aeca
yindaheng98 <yindaheng98@163.com>
2021-08-22 22:22:16 +0800

    测试几乎完成，更新版本

--- test_File.py
+++ test_File.py
@@ -1,28 +1,39 @@
+import os
 import asyncio
 import logging
 from datetime import timedelta
 
-from simplarchiver import Pair
+from simplarchiver import Pair, FilterDownloader
 from simplarchiver.example import FileFeeder, DirFeeder, WalkFeeder, ExtFilterFeeder, JustDownloader
+from simplarchiver.example import QBittorrentDownloader
+from test_secret import qb_data
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)-8s | %(name)-24s | %(message)s')
-
-def log(msg):
-    logging.info('test_Pair | %s' % msg)
-
 
 ttrss_feeders = [
     FileFeeder('./simplarchiver'),
     DirFeeder('./simplarchiver'),
     WalkFeeder('./simplarchiver'),
-    ExtFilterFeeder(FileFeeder('./simplarchiver'), '.py')
+    ExtFilterFeeder(FileFeeder(qb_data['torrent_dir']), '.torrent')
 ]
 
-just_downloaders = [JustDownloader(i) for i in range(1, 4)]
 
+class QBItemFilterDownloader(FilterDownloader):
+
+    async def filter(self, item):
+        if os.path.splitext(item)[1].lower() != '.torrent':
+            return None
+        return {
+            'torrent_files': item,
+            'save_path': qb_data['save_path_gen'](item),
+        }
+
+
+just_downloaders = [QBItemFilterDownloader(JustDownloader(i)) for i in range(1, 4)]
+just_downloaders = [QBItemFilterDownloader(QBittorrentDownloader(**qb_data['opt']))]
 pair = Pair(ttrss_feeders,
             just_downloaders,
             timedelta(seconds=5), 4, 8)
-log("pair.coroutine_once()")
+logging.info('pair.coroutine_once()')
 asyncio.run(pair.coroutine_once())
