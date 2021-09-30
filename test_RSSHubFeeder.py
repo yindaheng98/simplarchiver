3da3a513ec37d95eb8d4b50524c1f9194704ba52
yindaheng98 <yindaheng98@163.com>
2021-09-30 11:10:54 +0800

    ttrss和rsshub分开来测试

--- test_RSSHubFeeder.py
+++ test_RSSHubFeeder.py
@@ -17,17 +17,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 def log(msg):
     logging.info('test_Pair | %s' % msg)
 
-
-ttrss_feeders = [TTRSSHubLinkFeeder(
-    TTRSSCatFeeder(
-        cat_id=ttrss['cat_id'],
-        logger=logging.getLogger("test_TTRSSFeeder %s %d" % (ttrss['url'], ttrss['cat_id'])),
-        url=ttrss['url'],
-        username=ttrss['username'],
-        password=ttrss['password']),
-    logger=logging.getLogger("test_TTRSSHubLinkFeeder %s %d" % (ttrss['url'], ttrss['cat_id']))
-) for ttrss in ttrss_data]
-
 rsshub_feeders = [RSSHubFeeder(
     url=rsshub,
     logger=logging.getLogger("test_RSSHubFeeder %s" % rsshub),
@@ -46,24 +35,8 @@ just_downloaders = [JustDownloader(i) for i in range(1, 4)]
 eo_downloaders = [EnclosureOnlyDownloader(base_downloader=JustDownloader(i)) for i in range(4, 7)]
 ee_downloaders = [EnclosureExceptDownloader(base_downloader=JustDownloader(i)) for i in range(7, 10)]
 
-subprocess_downloaders = [
-    JustLogCallbackDownloader(
-        SubprocessDownloader(
-            lambda x: 'ping 127.0.0.1', 'gbk',
-            logger=logging.getLogger("test_SubprocessDownloader")),
-        logger=logging.getLogger("test_JustLogCallbackDownloader")),
-    UpdateDownloader(
-        base_downloader=SubprocessDownloader(
-            lambda x: 'ping 127.0.0.1', 'gbk',
-            logger=logging.getLogger("test_SubprocessDownloader")),
-        update_list_path="./test.json",
-        update_list_pair_gen=lambda i: (i['link'], i['pubDate']) if 'link' in i and 'pubDate' in i else (None, None),
-        logger=logging.getLogger("test_UpdateDownloader")
-    )
-]
-
-pair = Pair(ttrss_feeders + rsshub_feeders + rsshub_multipage_feeders,
-            just_downloaders + eo_downloaders + ee_downloaders + subprocess_downloaders,
+pair = Pair(rsshub_feeders + rsshub_multipage_feeders,
+            just_downloaders + eo_downloaders + ee_downloaders,
             timedelta(seconds=5), 4, 8)
 log("pair.coroutine_once()")
 asyncio.run(pair.coroutine_once())
---
+++ test_TTRSSFeeder.py
@@ -0,0 +1,52 @@
+import asyncio
+import logging
+from datetime import timedelta
+
+import httpx
+
+from simplarchiver import Pair
+from simplarchiver.example.rss import TTRSSCatFeeder, TTRSSHubLinkFeeder
+from simplarchiver.example import JustDownloader, SubprocessDownloader, JustLogCallbackDownloader
+from simplarchiver.example.file import UpdateDownloader
+from test_secret import ttrss_data
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def log(msg):
+    logging.info('test_Pair | %s' % msg)
+
+
+ttrss_feeders = [TTRSSHubLinkFeeder(
+    TTRSSCatFeeder(
+        cat_id=ttrss['cat_id'],
+        logger=logging.getLogger("test_TTRSSFeeder %s %d" % (ttrss['url'], ttrss['cat_id'])),
+        url=ttrss['url'],
+        username=ttrss['username'],
+        password=ttrss['password']),
+    logger=logging.getLogger("test_TTRSSHubLinkFeeder %s %d" % (ttrss['url'], ttrss['cat_id']))
+) for ttrss in ttrss_data]
+
+just_downloaders = [JustDownloader(i) for i in range(1, 4)]
+
+subprocess_downloaders = [
+    JustLogCallbackDownloader(
+        SubprocessDownloader(
+            lambda x: 'ping 127.0.0.1', 'gbk',
+            logger=logging.getLogger("test_SubprocessDownloader")),
+        logger=logging.getLogger("test_JustLogCallbackDownloader")),
+    UpdateDownloader(
+        base_downloader=SubprocessDownloader(
+            lambda x: 'ping 127.0.0.1', 'gbk',
+            logger=logging.getLogger("test_SubprocessDownloader")),
+        update_list_path="./test.json",
+        update_list_pair_gen=lambda i: (i['link'], i['pubDate']) if 'link' in i and 'pubDate' in i else (None, None),
+        logger=logging.getLogger("test_UpdateDownloader")
+    )
+]
+
+pair = Pair(ttrss_feeders,
+            just_downloaders + subprocess_downloaders,
+            timedelta(seconds=5), 4, 8)
+log("pair.coroutine_once()")
+asyncio.run(pair.coroutine_once())
