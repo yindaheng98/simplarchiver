5c10200340da1865baad81c63f298caf7c172dfd
yindaheng98 <yindaheng98@163.com>
2021-12-14 20:08:57 +0800

    文件名与文件内容相关

--- simplarchiver/example/rss/__init__.py
+++ simplarchiver/example/rss/__init__.py
@@ -1,4 +1,4 @@
-from .rss import RSSHubFeeder, RSSHubMultiPageFeeder
+from .rsshub import RSSHubFeeder, RSSHubMultiPageFeeder
 from .filter import TTRSSHubLinkFeeder, TTRSSHubLinkDownloader
 from .filter import EnclosureOnlyDownloader, EnclosureExceptDownloader
 from .ttrss import TTRSSGenFeeder, TTRSSCatFeeder
